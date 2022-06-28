e060456a0cdf9366b7079064db0a9be278f7f876
Rubinho Maroti <rubinho.maroti@gmail.com>
2022-06-28 11:13:14 -0300

    Adiciona index

---
+++ index.js
@@ -0,0 +1,7 @@
+const OAuthMiddleware = require('./middleware');
+const OAuthMiddlewareSingleton = require('./singleton');
+
+module.exports = {
+    OAuthMiddleware,
+    OAuthMiddlewareSingleton
+}
